cf679a2f0f1116dc27273c515ba746b13b7b0489
Mylestym <myles.merriweather@outlook.com>
2025-08-01 15:09:40 -0500

    Updated: Generation size

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from render.pygame_render import render
 import matplotlib.pyplot as plt
 
 def main():
-    width, height = 20, 20
+    width, height = 100, 100
     tile_names = list(TILES.keys())
     grid = create_grid(width, height, tile_names)  # pass tile_names, not TILES
 
